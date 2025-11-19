86bb01be62beb38f1512e8f94b5d6135120f11ce
syaafiudinm <altafpasallo12@gmail.com>
2025-11-19 11:13:04 +0800

    Fix vite build path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "https://himastat.sci.unhas.ac.id/",
     plugins: [
         laravel({
             input: "resources/js/app.jsx",
@@ -11,4 +10,13 @@ export default defineConfig({
         }),
         react(),
     ],
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
