e1ff0b397a6deaa7fe36295f1373470b172a3984
syaafiudinm <altafpasallo12@gmail.com>
2025-11-25 16:00:55 +0800

    test 1

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,15 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import { Link } from "@inertiajs/react";
 
 export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen gap-4 items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
             <div>
                 <Link href="/">
-                    <img src="/favicon.svg" alt="Logo" className="h-72 w-72 mb-6" />
+                    <img
+                        src="/favicon.svg"
+                        alt="Logo"
+                        className="h-72 w-72 mb-6"
+                    />
                 </Link>
             </div>
 
