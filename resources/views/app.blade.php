45fb27fb72f4012c6550a76b29fa1840556cb748
syaafiudinm <altafpasallo12@gmail.com>
2025-11-25 16:58:48 +0800

    fix logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,7 +38,12 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
 
     <!-- Icons -->
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+    <link rel="manifest" href="/site.webmanifest" />
 
     <!-- Scripts -->
     @routes
