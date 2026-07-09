a5d87c486c9ced8c6a9fe0d5dc30b93e5a676b73
Fariz <muhfarhanrizki@gmail.com>
2026-07-09 23:09:38 +0800

    add for contact

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use App\Models\Contact;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -31,9 +32,12 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+
             'auth' => [
                 'user' => $request->user(),
             ],
+
+            'contact' => fn () => Contact::first(),
         ];
     }
 }
