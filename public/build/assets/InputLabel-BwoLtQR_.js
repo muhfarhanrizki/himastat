023a601556f68bcb4bb1f7a56d353568faf1eec0
syaafiudinm <altafpasallo12@gmail.com>
2026-07-11 13:01:22 +0800

    fix : edit 404 page redirect

---
+++ public/build/assets/InputLabel-BwoLtQR_.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-CWHpQTnV.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
--- resources/js/Pages/Admin/404.jsx
+++ resources/js/Pages/Admin/404.jsx
@@ -37,7 +37,7 @@ export default function Error404() {
 
                     {/* Button */}
                     <Link
-                        href="/dashboard"
+                        href="/"
                         className="inline-flex items-center px-8 py-4 bg-gray-700 hover:bg-gray-800 rounded-md text-white font-medium transform hover:scale-105 transition-all duration-200 shadow-lg"
                     >
                         <svg
@@ -53,7 +53,7 @@ export default function Error404() {
                                 d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                             />
                         </svg>
-                        Kembali ke Dashboard
+                        Kembali ke Home
                     </Link>
                 </div>
             </div>
